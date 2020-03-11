c93ca4bbc7d239b910d40be4c131ced8263e22be
rlfslf111 <57978597+rlfslf111@users.noreply.github.com>
2020-03-11 17:18:41 +0900

    SWEA

--- 백준_1697_숨바꼭질_길민규.py
+++ 백준_1697_숨바꼭질_길민규.py
@@ -1,32 +1,30 @@
-n,k = map(int,input().split())
-cnt =0
-
-def bfs(n,k,cnt):
-    con =1
-    visited = set()
+def bfs(N,K,cnt):
+    visit = set()
     location = set()
-    location.add(n)
-    while(con):
-        if(k in location):
-            con =0
+    location.add(N)
+
+    while 1:
+        if K in location:
             return cnt
         else:
-            cnt+=1
-            temp=list()
-            while(location):
-                temp_loc =location.pop()
-                temp.append(temp_loc)
-                visited.add(temp_loc)
-            for i in range(len(temp)):
-                if(temp[i]+1 not in visited):
-                    if(temp[i]+1<=100000):
-                        location.add(temp[i]+1)
-                if(temp[i]-1 not in visited):
-                    if(temp[i]-1>=0):
-                        location.add(temp[i]-1)
-                if(temp[i]*2 not in visited):
-                    if(temp[i]*2<=100000):
-                        location.add(temp[i]*2)
+            cnt += 1
+            tmp = []
+            while location:
+                tmp_loc = location.pop()
+                tmp.append(tmp_loc)
+                visit.add(tmp_loc)
+            for i in range(len(tmp)):
+                if tmp[i] + 1 not in visit:
+                    if tmp[i]+1 <= 100000:
+                        location.add(tmp[i]+1)
+                if tmp[i] - 1 not in visit:
+                    if tmp[i]-1 >= 0:
+                        location.add(tmp[i]-1)
+                if tmp[i]*2 not in visit:
+                    if tmp[i]*2 <= 100000:
+                        location.add(tmp[i]*2)
 
-cnt = bfs(n,k,cnt)
+N, K = map(int,input().split())
+cnt = 0
+cnt = bfs(N,K,cnt)
 print(cnt)
