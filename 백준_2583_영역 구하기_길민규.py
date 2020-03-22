c2b287fe77c8fae7a789c01072e1b087108a87fb
rlfslf111 <57978597+rlfslf111@users.noreply.github.com>
2020-03-22 23:35:10 +0900

    SWEA

--- 백준_2583_영역 구하기_길민규.py
+++ 백준_2583_영역 구하기_길민규.py
@@ -1,41 +1,40 @@
-import sys
-sys.setrecursionlimit(1000000)
-
+from collections import deque
 dy = [-1,0,1,0]
 dx = [0,1,0,-1]
-def dfs(y,x):
+def bfs(y,x):
+    q = deque()
+    q.append((y,x))
     visit[y][x] = 1
-    for i in range(len(dy)):
-        ny = y + dy[i]
-        nx = x + dx[i]
-        if ny < 0 or ny >= Y or nx < 0 or nx >= X or visit[ny][nx] != 0:
-            continue
-        if board[ny][nx] == 0 and visit[ny][nx] == 0:
-            visit[ny][nx] = 1
-            cnt[0] += 1
-            dfs(ny,nx)
+    cnt = 1
+    while q:
+        y, x = q.popleft()
+        for i in range(len(dy)):
+            ny, nx = y + dy[i], x + dx[i]
+            if 0 <= ny < Y and 0 <= nx < X and board[ny][nx] == 0 and visit[ny][nx] == 0:
+                q.append((ny,nx))
+                cnt += 1
+                visit[ny][nx] = 1
+
+    area.append(cnt)
 
 Y, X, K = map(int,input().split())
 board = [[0]*X for _ in range(Y)]
 visit = [[0]*X for _ in range(Y)]
 
 for k in range(K):
-    x1, y1, x2, y2 = map(int,input().split())
-    for y in range(y1, y2):
-        for x in range(x1, x2):
-            board[y][x] = 1
+    x1,y1,x2,y2 = map(int,input().split())
+    for i in range(y1,y2):
+        for j in range(x1,x2):
+            board[i][j] = 1
 
-ans = []
-cnt = [1]
 lump = 0
+area = []
 for y in range(Y):
     for x in range(X):
-        if board[y][x] == 0 and visit[y][x] != 1:
-            dfs(y,x)
-            ans.append(cnt[0])
+        if board[y][x] == 0 and visit[y][x] == 0:
+            bfs(y,x)
             lump += 1
-            cnt[0] = 1
-
-ans.sort()
+            
+area.sort()
 print(lump)
-print(*ans)
+print(*area)
