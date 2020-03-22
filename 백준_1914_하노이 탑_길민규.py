dd1e94e4bdd69f75037360a6c6f77355379d9eba
길민규 <rlfslf111@naver.com>
2020-03-22 23:45:47 +0900

    SWEA

---
+++ 백준_1914_하노이 탑_길민규.py
@@ -0,0 +1,20 @@
+def hanoi(n,a,b,c):
+    if n==1:
+        print(a,c)
+    else:
+        hanoi(n-1,a,c,b)
+        print(a,c)
+        hanoi(n-1,b,a,c)
+N=int(input())
+cur=0
+i=0
+while i<N:
+    cur=(2*cur)+1
+    i+=1
+
+if N <= 20:
+    print(cur)
+    hanoi(N,1,2,3)
+
+if N > 20:
+    print(cur)
